9674e991d4d89fb2d1e9b27fd829b236601f4e66
DerPremiumgamer <pascal.liedloff@web.de>
2021-02-12 18:19:22 +0100

    Neuer Code Pascal

---
+++ src/main/java/TrexTest.java
@@ -0,0 +1,44 @@
+import java.awt.AWTException;
+import java.awt.Color;
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
+import java.util.Timer;
+import java.util.TimerTask;
+
+
+
+public class TrexTest {
+	
+	public static void main(String[] args) {
+		
+		Color graufarbe = new Color(83,83,83);
+		Timer timer = new Timer();
+		
+		timer.scheduleAtFixedRate(new TimerTask() {
+			
+			public void run() {
+				
+			try {
+		         Robot robot = new Robot();    
+		         Color Farbe = robot.getPixelColor(800,510);	
+		         
+		            if ((Farbe.equals(graufarbe))) {
+		            	System.out.println("Springen"); 
+						robot.keyPress(KeyEvent.VK_SPACE);
+						
+		            } 
+    
+		    } 
+			catch (AWTException e) {
+	            e.printStackTrace();
+			}
+			
+			}
+			
+		},0, 10 );
+		 
+	}
+	    
+}
+	
+
